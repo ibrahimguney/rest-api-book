5bd8bfedfe3cfc184b2d40f0438ed8510958b032
ML <ibrahimguney16@gmail.com>
2026-04-25 16:49:44 +0300

    Add users router and update root message

--- api/main.py
+++ api/main.py
@@ -1,15 +1,17 @@
 from fastapi import FastAPI
-from api.routes import students
+
 from api.database import Base, engine
 from api import models
+from api.routes import students, users
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="REST API Book")
 
 app.include_router(students.router, prefix="/students", tags=["Students"])
+app.include_router(users.router, prefix="/users", tags=["Users"])
 
 
 @app.get("/")
 def root():
-    return {"message": "API with PostgreSQL is running 🚀"}
+    return {"message": "API with PostgreSQL and JWT is running 🚀"}
