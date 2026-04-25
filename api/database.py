eef2e232d785eca445560b21951ef21b0b77572f
ML <ibrahimguney16@gmail.com>
2026-04-25 12:50:39 +0300

    Set up SQLAlchemy database connection

--- api/database.py
+++ api/database.py
@@ -1,1 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
+DATABASE_URL = "postgresql://admin:password@db:5432/university"
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
