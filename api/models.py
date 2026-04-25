bb30c81def3a1be98fa3d18db5eb56ae9b587046
ML <ibrahimguney16@gmail.com>
2026-04-25 16:42:00 +0300

    Add User model with email and hashed_password fields

--- api/models.py
+++ api/models.py
@@ -8,3 +8,11 @@ class Student(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
     department = Column(String, nullable=False)
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
