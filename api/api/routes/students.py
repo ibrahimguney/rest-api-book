9029979d4c0b447ebdc2fe586424a50e5c45491e
ML <ibrahimguney16@gmail.com>
2026-04-25 10:59:24 +0300

    Add endpoint to add a new student

--- api/api/routes/students.py
+++ api/api/routes/students.py
@@ -7,3 +7,8 @@ students = []
 @router.get("/")
 def get_students():
     return students
+
+@router.post("/")
+def add_student(student: dict):
+    students.append(student)
+    return {"message": "Student added", "data": student}
