1c380dcd90f5ea720f3c37f44c13525fcbce1ebb
ML <ibrahimguney16@gmail.com>
2026-04-25 16:44:34 +0300

    Add user creation and login schemas
    Added UserCreate and UserLogin models with email and password fields.

--- api/schemas.py
+++ api/schemas.py
@@ -1,6 +1,24 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
+
+# -------------------------
+# STUDENT
+# -------------------------
 
 class StudentCreate(BaseModel):
     name: str
     department: str
+
+
+# -------------------------
+# USER
+# -------------------------
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
