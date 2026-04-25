2dbcffb77508177165a0eba83bd1812ce2edc7c1
ML <ibrahimguney16@gmail.com>
2026-04-25 19:18:34 +0300

    Add response models for Student and User

--- api/schemas.py
+++ api/schemas.py
@@ -1,24 +1,30 @@
 from pydantic import BaseModel, EmailStr
 
-
-# -------------------------
-# STUDENT
-# -------------------------
-
 class StudentCreate(BaseModel):
     name: str
     department: str
 
+class StudentResponse(BaseModel):
+    id: int
+    name: str
+    department: str
 
-# -------------------------
-# USER
-# -------------------------
+    model_config = {"from_attributes": True}
 
 class UserCreate(BaseModel):
     email: EmailStr
     password: str
 
-
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
+
+class UserResponse(BaseModel):
+    id: int
+    email: EmailStr
+
+    model_config = {"from_attributes": True}
+
+class TokenResponse(BaseModel):
+    access_token: str
+    token_type: str
