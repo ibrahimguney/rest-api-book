c4f859aed29486eb12b007c6cc3b591024984b2e
ML <ibrahimguney16@gmail.com>
2026-04-25 12:50:09 +0300

    Set up FastAPI with students route and database

--- api/main.py
+++ api/main.py
@@ -1,1 +1,15 @@
+from fastapi import FastAPI
+from api.routes import students
+from api.database import Base, engine
+from api import models
 
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="REST API Book")
+
+app.include_router(students.router, prefix="/students", tags=["Students"])
+
+
+@app.get("/")
+def root():
+    return {"message": "API with PostgreSQL is running 🚀"}
