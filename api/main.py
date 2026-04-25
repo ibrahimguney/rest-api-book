9caa6624a6856ea2ea989c7c8c25bb2ce1e40337
ML <ibrahimguney16@gmail.com>
2026-04-25 11:52:11 +0300

    Update root endpoint message for PostgreSQL

--- api/main.py
+++ api/main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from api.routes import students
+from api.database import engine, Base
+from api import models
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="REST API Book")
 
@@ -7,4 +11,4 @@ app.include_router(students.router, prefix="/students")
 
 @app.get("/")
 def root():
-    return {"message": "API is running 🚀"}
+    return {"message": "API with PostgreSQL is running 🚀"}
